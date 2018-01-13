7b948fd6f43396430d8db4199b304249589ae2ba
Antonio Sotelo <asotel6@gmail.com>
2018-01-13 15:36:05 -0600

    Added asp.net application files

---
+++ configurations/dbconntest/Default.aspx
@@ -0,0 +1,13 @@
+  <!--Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default"-->
+
+       <title></title>
+
+        <form id="form1" runat="server">
+
+         <div>
+
+       <asp:button id="btnTestDb" onclick="btnTestDb_Click" runat="server" text="Test Database Connection"/>
+
+      </div>
+
+       </form>
---
+++ configurations/dbconntest/Default.aspx.cs
@@ -0,0 +1,33 @@
+using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Web;
+ using System.Web.UI;
+ using System.Web.UI.WebControls;
+ using System.Configuration;
+ using System.Data.SqlClient;
+ using System.Data;
+ public partial class _Default : System.Web.UI.Page
+ {
+ protected void Page_Load(object sender, EventArgs e)
+ {
+ }
+ protected void btnTestDb_Click(object sender, EventArgs e)
+ {try
+{
+ SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["EarthConn"].ConnectionString);
+ connection.Open();
+ if ((connection.State &amp; ConnectionState.Open) &gt; 0)
+ {
+ Response.Write("Connection OK!");
+ connection.Close();
+ }
+ else
+ {
+ Response.Write("No Connection!");
+ }
+ }catch
+{
+Response.Write("No Connection!");
+}
+ }    
