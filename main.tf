c0d8655cc93382662ab6acbed5e37ef4ca71d2a1
Antonio Sotelo <asotel6@gmail.com>
2018-01-14 12:05:17 -0600

    added sql fw rule

--- main.tf
+++ main.tf
@@ -207,6 +207,15 @@ resource "azurerm_sql_server" "server" {
   }
 }
 
+
+resource "azurerm_sql_firewall_rule" "fw" {
+  name                = "dbdemo1firewallrules"
+  resource_group_name = "asotelovmssdemo"
+  server_name         = "${azurerm_sql_server.server.name}"
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
 output "windows_vm_public_name" {
   value = "${module.windowsservers.public_ip_dns_name}"
 }
