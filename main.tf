bf937d39cdb1e0cad61343005ce2fc58a697df1e
Antonio Sotelo <asotel6@gmail.com>
2018-01-15 06:36:36 -0600

    modified outputs

--- main.tf
+++ main.tf
@@ -220,15 +220,15 @@ resource "azurerm_sql_firewall_rule" "fw" {
 }
 
 //Outputs
-output "windows_vm_public_name" {
+output "jumpbox_public_name" {
   value = "${module.windowsservers.public_ip_dns_name}"
 }
 
-output "windows_vm_public_ip" {
+output "jumpbox_public_ip" {
   value = "${module.windowsservers.public_ip_address}"
 }
 
-output "windows_vm_private_ips" {
+output "jumpbox_private_ips" {
   value = "${module.windowsservers.network_interface_private_ip}"
 }
 
