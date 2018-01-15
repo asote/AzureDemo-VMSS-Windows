b78ad182a6d79968857ea887ad780d451e5a7f6a
Antonio Sotelo <asotel6@gmail.com>
2018-01-15 06:51:07 -0600

    fixed powershell script

--- configurations/Configure-WebServer.ps1
+++ configurations/Configure-WebServer.ps1
@@ -6,9 +6,9 @@ Add-WindowsFeature -Name web-server, Web-Asp-Net45, Web-Mgmt-Console
 
 rm -Path c:\inetpub\wwwroot\* -Recurse
 
-wget -uri https://raw.githubusercontent.com/asote/AzureDemo-VMSS-Windows/dbconn/configurations/tests/Default.aspx -OutFile C:\inetpub\wwwroot\Default.aspx
-wget -uri https://raw.githubusercontent.com/asote/AzureDemo-VMSS-Windows/dbconn/configurations/tests/Default.aspx.cs -OutFile c:\inetpub\wwwroot\Default.aspx.cs
-wget -uri https://raw.githubusercontent.com/asote/AzureDemo-VMSS-Windows/dbconn/configurations/tests/web.config -OutFile c:\inetpub\wwwroot\Web.config
+wget -uri https://raw.githubusercontent.com/asote/AzureDemo-VMSS-Windows/master/configurations/tests/Default.aspx -OutFile C:\inetpub\wwwroot\Default.aspx
+wget -uri https://raw.githubusercontent.com/asote/AzureDemo-VMSS-Windows/master/configurations/tests/Default.aspx.cs -OutFile c:\inetpub\wwwroot\Default.aspx.cs
+wget -uri https://raw.githubusercontent.com/asote/AzureDemo-VMSS-Windows/master/configurations/tests/web.config -OutFile c:\inetpub\wwwroot\Web.config
 
 $endDTM = (Get-Date)
 
